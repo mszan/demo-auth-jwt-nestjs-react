aa4823fcbbf70add8ef0105271833f04d2f0bf69
Dawid Mszanowski <dmszanowski@icloud.com>
2025-05-28 09:16:48 +0200

    Add description to validate method

--- services/backend/source/src/modules/auth/strategies/jwt.strategy.ts
+++ services/backend/source/src/modules/auth/strategies/jwt.strategy.ts
@@ -28,7 +28,13 @@ export class JwtStrategy extends PassportStrategy(Strategy, "jwt") {
     });
   }
 
-  // todo: add description
+  /**
+   * Passport first verifies the JWT's signature and decodes the JSON. It then invokes this method passing the decoded JSON as its single parameter.
+   * Based on the way JWT signing works, we're guaranteed that we're receiving a valid token that we have previously signed and issued to a valid user.
+   *
+   * As a result of all this, our response to the validate() callback is trivial: we simply return an object containing the payload.
+   * Recall again that Passport will build a user object based on the return value of our validate() method, and attach it as a property on the Request object.
+   */
   validate(payload: JwtPayload) {
     return payload;
   }
